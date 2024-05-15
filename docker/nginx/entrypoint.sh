2f9cd601cef985b168001a0261748397bde4dc34
miyataro0314 <miyamoto.webdev@gmail.com>
2024-05-15 16:39:13 +0900

    force_ssl修正、nginx設定調整

--- docker/nginx/entrypoint.sh
+++ docker/nginx/entrypoint.sh
@@ -2,11 +2,13 @@
 
 # 初回の証明書取得が必要な場合に対応
 if [ ! -f /etc/letsencrypt/live/moto-tokyo.com/fullchain.pem ]; then
-    certbot --nginx -d moto-tokyo.com --email miyamoto.webdev@gmail.com --agree-tos --no-eff-email
+  nginx -c /etc/nginx/nginx.conf
+  certbot certonly --nginx -d moto-tokyo.com --email miyamoto.webdev@gmail.com --agree-tos --no-eff-email --force-renewal
+  nginx -s stop
 fi
 
 # cronサービスを開始
 service cron start
 
 # Nginxを起動
-nginx -g "daemon off;" -c /etc/nginx/nginx.conf
+nginx -g "daemon off;" -c /etc/nginx/nginx_permanent.conf
