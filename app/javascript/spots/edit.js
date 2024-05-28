efe0dbf25421e4783519fb24e46393f54c186be6
miyataro0314 <miyamoto.webdev@gmail.com>
2024-05-29 00:37:53 +0900

    駐車場カードのレイアウト修正、ページネーション修正

--- app/javascript/spots/edit.js
+++ app/javascript/spots/edit.js
@@ -1,8 +1,13 @@
 const parkingSelect = document.getElementById('parking-select');
 const parkingLimitationSelect = document.getElementById('parking-limitaiton-select');
 
+// 初期表示対応用
+if (parkingSelect.value == 'nothing' || parkingSelect.value == 'unknown') {
+  parkingLimitationSelect.disabled = true;
+}
+
 parkingSelect.addEventListener('change', function() {
-  if (this.value == '' || this.value == 'nothing') {
+  if (this.value == '' || this.value == 'nothing' || this.value == 'unknown') {
     parkingLimitationSelect.value = ''
     parkingLimitationSelect.disabled = true;
   } else {
