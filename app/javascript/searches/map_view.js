bd3acdb4c6361e93f00800b147a8ce02cdacbcb7
miyataro0314 <miyamoto.webdev@gmail.com>
2024-06-09 22:30:21 +0900

    マップビュー表示部分のみマーカー表示

--- app/javascript/searches/map_view.js
+++ app/javascript/searches/map_view.js
@@ -1,18 +1,21 @@
-initMap();
+let markers = []
 
-function initMap() {
+initMap(markers);
+
+function initMap(markers) {
   const centerPosition = { lat: 35.6895, lng: 139.6917 };
   const map = new google.maps.Map(document.getElementById('map'), {
     mapId: '<%= Rails.application.credentials.google_maps[:map_id] %>', 
     center: centerPosition,
-    zoom: 12,
+    zoom: 14,
     gestureHandling: 'greedy'
   });
 
-  reloadMarkers(map, centerPosition);
+  loadMarkers(map, centerPosition, markers);
+  map.addListener('idle', () => updateMarkersDisplay(map));
 }
 
-function reloadMarkers(map, position) {
+function loadMarkers(map, position, markers) {
   fetch('/api/v1/searches/load_map_data',{
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
@@ -27,64 +30,72 @@ function reloadMarkers(map, position) {
       const parkings = data.parkings;
       const infoWindow = new google.maps.InfoWindow();
 
-      setSpotMarkers(map, spots, infoWindow);
-      setParkingMarkers(map, parkings, infoWindow);
+      setSpotMarkers(map, spots, infoWindow, markers);
+      setParkingMarkers(map, parkings, infoWindow, markers);
+      updateMarkersDisplay(map);
   }).catch(error => {
     console.log(error);
   })
 }
 
-function setSpotMarkers(map, spots, infoWindow) {
-  spots.forEach((spot, i) => {
-    setTimeout(() => {
-      const regex = /POINT \(([0-9]+.[0-9]+) ([0-9]+.[0-9]+)\)/;
-      const match = spot.spot_detail.coordinate.match(regex);
-      const spotPosition = { lat: parseFloat(match[2]),
-                            lng: parseFloat(match[1]) };
-      const spotMarker = new google.maps.marker.AdvancedMarkerElement({
-        map,
-        position: spotPosition,
-        title: spot.name
-      });
-
-      spotMarker.addListener('click', (function(name, marker) {
-        return function() {
-          infoWindow.setContent(name);
-          infoWindow.open(map, marker);
-        };
-      })(spot.name, spotMarker));
-    }, i * 10);
+function updateMarkersDisplay(map) {
+  markers.forEach(function(marker){
+    if(map.getBounds().contains(marker.position)){
+      marker.setMap(map);
+    }else{
+      marker.setMap(null);
+    }
   });
 }
 
-function setParkingMarkers(map, parkings, infoWindow) {
+function setSpotMarkers(map, spots, infoWindow, markers) {
+  spots.forEach((spot, i) => {
+    const regex = /POINT \(([0-9]+.[0-9]+) ([0-9]+.[0-9]+)\)/;
+    const match = spot.spot_detail.coordinate.match(regex);
+    const spotPosition = { lat: parseFloat(match[2]),
+                          lng: parseFloat(match[1]) };
+    const spotMarker = new google.maps.marker.AdvancedMarkerElement({
+      position: spotPosition,
+      title: spot.name
+    });
+
+    spotMarker.addEventListener('click', (function(name, marker) {
+      return function() {
+        infoWindow.setContent(name);
+        infoWindow.open(map, marker);
+      };
+    })(spot.name, spotMarker));
+
+    markers.push(spotMarker)
+  });
+}
+
+function setParkingMarkers(map, parkings, infoWindow, markers) {
   
   parkings.forEach((parking, i) => {
-    setTimeout(() => {
-      const regex = /POINT \(([0-9]+.[0-9]+) ([0-9]+.[0-9]+)\)/;
-      const match = parking.coordinate.match(regex);
-      const parkingPosition = { lat: parseFloat(match[2]),
-                                lng: parseFloat(match[1]) };
-      const blueMarker = new google.maps.marker.PinView({
-        background: "#1b73e8",
-        borderColor: "#0b4087",
-        glyphColor: "#0b4087"
-      });
-      
-      parkingMarker = new google.maps.marker.AdvancedMarkerElement({
-        map,
-        position: parkingPosition,
-        content: blueMarker.element
-      });
-        
-      parkingMarker.addListener('click', (function(name, marker) {
-        return function() {
-          infoWindow.setContent(name);
-          infoWindow.open({
-            anchor: marker
-          });
-        }
-      })(parking.name, parkingMarker));
-    }, i * 5);
+    const regex = /POINT \(([0-9]+.[0-9]+) ([0-9]+.[0-9]+)\)/;
+    const match = parking.coordinate.match(regex);
+    const parkingPosition = { lat: parseFloat(match[2]),
+                              lng: parseFloat(match[1]) };
+    const blueMarker = new google.maps.marker.PinView({
+      background: "#1b73e8",
+      borderColor: "#0b4087",
+      glyphColor: "#0b4087"
+    });
+    
+    parkingMarker = new google.maps.marker.AdvancedMarkerElement({
+      position: parkingPosition,
+      content: blueMarker.element
+    });
+
+    parkingMarker.addEventListener('click', (function(name, marker) {
+      return function() {
+        infoWindow.setContent(name);
+        infoWindow.open(map, marker);
+      }
+    })(parking.name, parkingMarker));
+
+    markers.push(parkingMarker)
+
   });
 }
